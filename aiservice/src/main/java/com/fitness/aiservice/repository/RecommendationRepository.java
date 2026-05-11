2d55adddde4f02f3329a049a219e43481cab2125
Peter <peterhalim50@gmail.com>
2026-05-11 22:05:58 +0300

    setting up the ai-service

---
+++ aiservice/src/main/java/com/fitness/aiservice/repository/RecommendationRepository.java
@@ -0,0 +1,19 @@
+package com.fitness.aiservice.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fitness.aiservice.model.Recommendations;
+
+@Repository
+public interface RecommendationRepository extends MongoRepository<Recommendations, String>{
+
+    List<Recommendations> findByUserId(String userId);
+
+     Optional<Recommendations> findByActivityId(String activityId);
+     // 4:00
+
+}
